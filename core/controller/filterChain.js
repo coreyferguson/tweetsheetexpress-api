f96dba6e6d4d7e9d5618304bf3d4592ca790f655
Corey Ferguson <corey.t.ferguson@gmail.com>
2017-11-10 08:27:40 -0800

    each link in filterChain to return promise

--- core/controller/filterChain.js
+++ core/controller/filterChain.js
@@ -9,19 +9,20 @@ class FilterChain {
     this.chain = options.chain || chain;
   }
 
-  wrapInChain(event, callback) {
-    const response = { statusCode: 200 };
-    let index = 0;
-    const next = () => {
-      const nextFinal =
-        (index < this.chain.length-1)
-        ? next
-        : () => {
-          callback(response);
-        };
-        this.chain[index++].apply(event, response, nextFinal);
-    }
-    next();
+  wrapInChain(event) {
+      const response = { statusCode: 200 };
+      let index = 0;
+      const callback = () => Promise.resolve(response);
+      const applyNextLink = () => {
+        if (index === this.chain.length) return callback();
+        const promise = this.chain[index++].apply(event, response);
+        if (!promise) return callback();
+        return promise.then(shouldContinue => {
+          if (shouldContinue) return applyNextLink();
+          else return callback();
+        });
+      };
+      return applyNextLink();
   }
 
 }
--- test/spec/core/controller/filterChain.unit.js
+++ test/spec/core/controller/filterChain.unit.js
@@ -7,25 +7,111 @@ describe.only('filterChain unit tests', () => {
   it('apply all links in chain until callback', () => {
     let filterChain = new FilterChain({
       chain: [
-        { apply: (event, response, next) => next() },
-        { apply: (event, response, next) => next() }
+        { apply: sinon.spy((event, response) => Promise.resolve(true)) },
+        { apply: sinon.spy((event, response) => Promise.resolve(true)) }
       ]
     });
-    const event = {};
-    return filterChain.wrapInChain(event, response => {
-      expect(response).to.eql({
-        statusCode: 200
+    return expect(filterChain.wrapInChain({}))
+      .to.eventually.eql({ statusCode: 200 })
+      .then(() => {
+        expect(filterChain.chain[0].apply).to.be.calledOnce;
+        expect(filterChain.chain[1].apply).to.be.calledOnce;
       });
-    });
   });
 
-  xit('apply each link until one does not call `next` function', () => {
+  it('apply each link until one resolves `false`', () => {
     let filterChain = new FilterChain({
       chain: [
-        { apply: sinon.spy((event, response, next) => {}) },
-        { apply: sinon.spy((event, response, next) => next()) }
+        { apply: sinon.spy((event, response) => Promise.resolve(true)) },
+        { apply: sinon.spy((event, response) => Promise.resolve(false)) },
+        // will not be called because previous link in chain resolves `false`
+        { apply: sinon.spy((event, response) => Promise.resolve(true)) }
       ]
     });
+    return expect(filterChain.wrapInChain({}))
+      .to.eventually.eql({ statusCode: 200 }).then(() => {
+        expect(filterChain.chain[0].apply).to.be.calledOnce;
+        expect(filterChain.chain[1].apply).to.be.calledOnce;
+        expect(filterChain.chain[2].apply).to.not.be.called;
+      });
+  });
+
+  it('apply each link until one does not return promise', () => {
+    let filterChain = new FilterChain({
+      chain: [
+        { apply: sinon.spy((event, response) => Promise.resolve(true)) },
+        { apply: sinon.spy((event, response) => {}) },
+        // will not be called because previous link in chain resolves `false`
+        { apply: sinon.spy((event, response) => Promise.resolve(true)) }
+      ]
+    });
+    return expect(filterChain.wrapInChain({}))
+      .to.eventually.eql({ statusCode: 200 }).then(() => {
+        expect(filterChain.chain[0].apply).to.be.calledOnce;
+        expect(filterChain.chain[1].apply).to.be.calledOnce;
+        expect(filterChain.chain[2].apply).to.not.be.called;
+      });
+  });
+
+  it('event passed to each link', () => {
+    const eventPropertyValidator = (event, response) => {
+      expect(event.testPropertyLabel).to.equal('testPropertyValue');
+      return Promise.resolve(true);
+    };
+    let filterChain = new FilterChain({
+      chain: [
+        { apply: eventPropertyValidator },
+        { apply: eventPropertyValidator }
+      ]
+    });
+    return filterChain.wrapInChain({ testPropertyLabel: 'testPropertyValue' });
+  });
+
+  it('response default values', () => {
+    const responsePropertyValidator = (event, response) => {
+      expect(response.statusCode).to.equal(200);
+      return Promise.resolve(true);
+    };
+    let filterChain = new FilterChain({
+      chain: [
+        { apply: responsePropertyValidator },
+        { apply: responsePropertyValidator }
+      ]
+    });
+    return filterChain.wrapInChain();
+  });
+
+  it('response shared by reference to all links in chain', () => {
+    let filterChain = new FilterChain({
+      chain: [
+        {
+          apply: (event, response) => {
+            expect(response.callCount).to.be.undefined;
+            response.callCount = 1;
+            return Promise.resolve(true);
+          }
+        },
+        {
+          apply: (event, response) => {
+            expect(response.callCount).to.equal(1);
+            response.callCount++;
+            return Promise.resolve(true);
+          }
+        },
+        {
+          apply: (event, response) => {
+            expect(response.callCount).to.equal(2);
+            response.callCount++;
+            return Promise.resolve(true);
+          }
+        }
+      ]
+    });
+    return expect(filterChain.wrapInChain())
+      .to.eventually.eql({
+        statusCode: 200,
+        callCount: 3
+      });
   });
 
 });
